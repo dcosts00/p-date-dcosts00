1ec430b55a837a038755e08bd2d10febae355af8
dcosts00 <dcosts00@estudiantes.unileon.es>
2017-03-31 00:03:35 +0200

    Quinto commit

---
+++ src/practicaDate/MainDate.java
@@ -0,0 +1,24 @@
+/** 
+ * @author Dona Costa Suarez 
+ * @version 2017
+ */
+package practicaDate;
+
+
+public class MainDate
+{
+  public static void main(String args[]){
+      Date today;
+      today = new Date (22,3,2017);
+      System.out.println("¿El numero de dias es correcto?"+today.monthsDayIsRight());
+      System.out.println("El nombre del mes es: "+today.getMonthsName());
+      System.out.println("El numero de dias del mes es: "+today.getNumberDaysMonth());
+      System.out.println("La estacion a la que pertenece es: "+today.getMonthSeason());
+      System.out.println("Los dias que faltan para que acabe el mes: "+today.printDaysLeft());
+      System.out.println("Los meses que tienen el mismo numero de dias son: "+ today.printMonthsSameAmountDays());
+      System.out.println("Los meses que faltan para que acabe el año son: "+today.printMonthsLeft());
+      System.out.println("Los dias que han pasado desde que empezo el anio son: "+today.countDays());
+      System.out.println("Numero de intentos: "+today.getNumGuess());
+      System.out.println("El dia de la semana es: "+today.getWeekDay());
+  }
+}
